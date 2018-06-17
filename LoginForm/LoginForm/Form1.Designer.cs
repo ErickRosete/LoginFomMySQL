d8edf44fef8629ac767d1539ef98224d0491b2e9
Erick Rosete Beas <erickrosetebeas@hotmail.com>
2018-06-16 23:12:51 -0700

    Initial Commit - Visual Studio

---
+++ LoginForm/LoginForm/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace LoginForm
+{
+    partial class Login_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.login_btn = new System.Windows.Forms.Button();
+            this.username_txt = new System.Windows.Forms.TextBox();
+            this.username_lbl = new System.Windows.Forms.Label();
+            this.password_lbl = new System.Windows.Forms.Label();
+            this.password_txt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // login_btn
+            // 
+            this.login_btn.Location = new System.Drawing.Point(75, 358);
+            this.login_btn.Name = "login_btn";
+            this.login_btn.Size = new System.Drawing.Size(101, 23);
+            this.login_btn.TabIndex = 0;
+            this.login_btn.Text = "Login";
+            this.login_btn.UseVisualStyleBackColor = true;
+            this.login_btn.Click += new System.EventHandler(this.login_btn_Click);
+            // 
+            // username_txt
+            // 
+            this.username_txt.Location = new System.Drawing.Point(134, 101);
+            this.username_txt.Name = "username_txt";
+            this.username_txt.Size = new System.Drawing.Size(100, 20);
+            this.username_txt.TabIndex = 1;
+            // 
+            // username_lbl
+            // 
+            this.username_lbl.AutoSize = true;
+            this.username_lbl.Location = new System.Drawing.Point(41, 104);
+            this.username_lbl.Name = "username_lbl";
+            this.username_lbl.Size = new System.Drawing.Size(55, 13);
+            this.username_lbl.TabIndex = 3;
+            this.username_lbl.Text = "Username";
+            // 
+            // password_lbl
+            // 
+            this.password_lbl.AutoSize = true;
+            this.password_lbl.Location = new System.Drawing.Point(43, 141);
+            this.password_lbl.Name = "password_lbl";
+            this.password_lbl.Size = new System.Drawing.Size(53, 13);
+            this.password_lbl.TabIndex = 4;
+            this.password_lbl.Text = "Password";
+            // 
+            // password_txt
+            // 
+            this.password_txt.Location = new System.Drawing.Point(134, 138);
+            this.password_txt.Name = "password_txt";
+            this.password_txt.PasswordChar = '*';
+            this.password_txt.Size = new System.Drawing.Size(100, 20);
+            this.password_txt.TabIndex = 6;
+            // 
+            // Login_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 393);
+            this.Controls.Add(this.password_txt);
+            this.Controls.Add(this.password_lbl);
+            this.Controls.Add(this.username_lbl);
+            this.Controls.Add(this.username_txt);
+            this.Controls.Add(this.login_btn);
+            this.Name = "Login_Form";
+            this.Text = "Login Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button login_btn;
+        private System.Windows.Forms.TextBox username_txt;
+        private System.Windows.Forms.Label username_lbl;
+        private System.Windows.Forms.Label password_lbl;
+        private System.Windows.Forms.TextBox password_txt;
+    }
+}
+
---
+++ LoginForm/LoginForm/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace LoginForm
+{
+    public partial class Login_Form : Form
+    {
+        public Login_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void login_btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Connection
+                string myConnection = "datasource=localhost;port=3306;username=root;password=root";
+                MySqlConnection myConn = new MySqlConnection(myConnection);
+
+                //Select
+                string select;
+                select = "select * from database.users where Name = '" + this.username_txt.Text + "' ";
+                select += "and password = '" + this.password_txt.Text + "';";
+                MySqlCommand SelectCommand = new MySqlCommand(select, myConn);
+
+                myConn.Open();
+                //Reading
+                MySqlDataReader myReader;
+                myReader = SelectCommand.ExecuteReader();
+
+                bool userAvailable = false; 
+                while (myReader.Read())
+                    userAvailable = true;
+     
+                if(userAvailable)
+                    MessageBox.Show("Access Granted");
+                else
+                    MessageBox.Show("Incorrect username or password");
+     
+                myConn.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+    }
+}
